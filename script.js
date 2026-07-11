920a8da9e913a7156684c887eb445a892e323102
Santiago Quintero Orozco <quinte0317@gmail.com>
2026-07-11 20:07:57 +0200

    Crear estructura inicial del portafolio

---
+++ script.js
@@ -0,0 +1,7 @@
+const boton = document.getElementById("mensaje-btn");
+
+boton.addEventListener("click", function () {
+
+    alert("¡Hola Santiago! Acabas de ejecutar tu primer evento con JavaScript 🎉");
+
+});
