25dfc0bf8cfbeed95996f24fdaae12268df2850e
Santiago Quintero Orozco <quinte0317@gmail.com>
2026-07-13 14:01:49 +0200

    Finaliza versión 1.0 del portafolio

--- script.js
+++
@@ -1,7 +0,0 @@
-const boton = document.getElementById("mensaje-btn");
-
-boton.addEventListener("click", function () {
-
-    alert("¡Hola Santiago! Acabas de ejecutar tu primer evento con JavaScript 🎉");
-
-});
